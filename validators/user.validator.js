dd10d04a5cda12aceefd44f4461005c0ae22b2da
Emelinda Méndez <emendez@ecomac.cl>
2023-05-01 21:06:19 -0400

    refactoring of validations

--- validators/user.validator.js
+++ validators/user.validator.js
@@ -2,32 +2,30 @@ const { check } = require('express-validator');
 const validateResults = require('../middlewares/validator');
 const User = require('../models/user');
 
+const nameValidation        = check('name').notEmpty();
+const lastNameValidation    = check('lastname').notEmpty();
+const passwordValidation    = check('password').isLength({ min: 6 });
+const emailValidation       = check('email').isEmail();
+const avatarValidation      = check('avatar').notEmpty();
+const roleValidation        = check('role').isIn(['ADMIN', 'USER']);
+
 const createValidation = [
-    check('name')
-        .notEmpty(),
-
-    check('lastname')
-        .notEmpty(),
-
-    check('password')
-        .isLength({ min: 6 }),
-
-    check('email')
-        .isEmail()
-        .custom( (email) => {//validation unique in database
-            return User.findOne({ where: { email } })
-            .then(() => {
-            return Promise.reject('Email already taken');
-            })
-        }),
-
-    check('avatar')
-        .notEmpty(),
-
-    check('role')
-        .isIn(['ADMIN', 'USER']),
-
+    nameValidation,
+    lastNameValidation,
+    passwordValidation.notEmpty(),
+    emailValidation
+    .custom(async (email) => {
+        const existingUser =await User.findOne({ email }); 
+        if (existingUser) {
+            throw new Error('Email already in use')
+        }
+    }),
+    avatarValidation,
+    roleValidation,
     (req, res, next) => validateResults(req, res, next)
 ];
+
+
+
 
 module.exports = { createValidation };
